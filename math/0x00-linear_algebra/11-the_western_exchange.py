2bcc025a54655164965fe21639ee9adc693543e6
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-15 08:51:08 -0500

    Exercise 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Module The Western Exchange: Calculates the transpose of a numpy array"""
+
+
+def np_transpose(matrix):
+    """Calculates the transpose of a numpy array"""
+
+    return matrix.copy().transpose()
