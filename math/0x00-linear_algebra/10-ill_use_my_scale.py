312e6c201180ab952f860aef0b5e526f7d6593c6
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-15 08:41:35 -0500

    Exercise 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+"""Module I’ll Use My Scale : Calculates the shape of numpy array"""
+
+
+def np_shape(matrix):
+    """Calculates the shape of numpy array"""
+
+    return matrix.shape
