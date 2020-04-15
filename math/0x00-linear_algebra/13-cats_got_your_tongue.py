528560f3fffc64d21a82e2d17533c8bbdd27d81f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-15 09:37:20 -0500

    Change import in exercise 13

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import numpy as np
 """Module Cat's Got Your Tongue:
     Concatenates two matrices along a specific axis"""
+
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
