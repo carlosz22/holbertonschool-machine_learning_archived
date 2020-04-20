101c63e1ab244a134f6df11ba539ddff208c2888
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-20 15:38:16 -0500

    Add exercise 6

---
+++ math/0x01-plotting/6-bars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4,3))
+
+width = 0.5
+
+plt.ylabel('Quantity of Fruit')
+plt.title('Number of Fruit per Person')
+
+ind = np.arange(fruit.shape[1])
+color_list = ['red', 'yellow', '#ff8000', '#ffe5b4']
+fruit_list = ['apples', 'bananas', 'oranges', 'peaches']
+
+for i in range(fruit.shape[0]):
+  plt.bar(ind, fruit[i],
+    bottom = np.sum(fruit[:i], axis = 0),
+    color = color_list[i % len(color_list)],
+    width=width, label=fruit_list[i])
+
+plt.xticks(ind, ('Farrah', 'Fred', 'Felicia'))
+plt.yticks(np.arange(0, 81, 10))
+plt.legend()
+
+plt.show()
