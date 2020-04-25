bb82bc17ce70780f863ee320635786b6e4e3d66b
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-25 14:42:16 -0500

    Add exercise 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Differentiates a polynomial"""
+
+
+def poly_derivative(poly):
+    """Differentiates a polynomial"""
+
+    if (type(poly) is not list or len(poly) < 1):
+        return None
+
+    derivative_coef = [poly[i] * i for i in range(len(poly))]
+
+    return derivative_coef[1:]
