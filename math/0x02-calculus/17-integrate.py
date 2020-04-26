a825e3f572c7f59c1b67e5f4053b733982e57204
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-26 16:38:09 -0500

    Add exercise 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Integrates a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """Integrates a polynomial"""
+
+    if (type(poly) is not list or len(poly) < 1
+            or type(C) is not int):
+        return None
+
+    integrate_coef = [poly[i] / (i + 1) for i in range(len(poly))]
+    integrate_coef.insert(0, C)
+    integrate_coef_fixed = [int(i) if i % 1 == 0 else i
+                            for i in integrate_coef]
+
+    return integrate_coef_fixed
