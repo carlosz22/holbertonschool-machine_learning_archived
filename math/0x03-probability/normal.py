9196930714b326501dc5cf6fcdce46a4fa96f52f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-04-30 15:56:30 -0500

    Add exercise 7

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -23,3 +23,13 @@ class Normal:
                 self.mean = float(sum(data) / len(data))
                 var = sum([((x - self.mean)) ** 2 for x in data])/len(data)
                 self.stddev = var ** 0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+
+        return (z * self.stddev) + self.mean
