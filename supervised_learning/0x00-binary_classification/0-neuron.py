e7b1cd25a16cf277b7d10b0e55309acc0dda5693
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-06 16:06:44 -0500

    Add exercise 0

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Defines a Neuron for binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """Class Neuron"""
+
+    def __init__(self, nx):
+        """Constructor method"""
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.W = np.random.randn(*(1, nx))
+        self.b = 0
+        self.A = 0
