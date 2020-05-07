Fix dZ1 calculation in exercise 13

supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -79,17 +79,16 @@ class NeuralNetwork:
         y_hat = self.__A2.round().astype(int)
         return y_hat, cost
 
-
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """Performs gradient descent"""
         m = Y.shape[1]
         dZ2 = A2 - Y
         db2 = 1/m * np.sum(dZ2, axis=1, keepdims=True)
-        dW2 = 1/m * np.matmul(dZ2, A1.T)
-        self.__b2 = self.__b2 - alpha * db2
-        self.__W2 = self.__W2 - alpha * dW2
-        dZ1 = np.matmul(self.__W2.T, dZ2) * A1
+        dW2 = 1/m * np.matmul(dZ2, A1.T) 
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
         db1 = 1/m * np.sum(dZ1, axis=1, keepdims=True)
         dW1 = 1/m * np.matmul(dZ1, X.T)
+        self.__b2 = self.__b2 - alpha * db2
+        self.__W2 = self.__W2 - alpha * dW2
         self.__b1 = self.__b1 - alpha * db1
         self.__W1 = self.__W1 - alpha * dW1