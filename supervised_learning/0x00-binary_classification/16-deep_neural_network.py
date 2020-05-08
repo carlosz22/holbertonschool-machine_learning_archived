c27eec2f6fb25051447c0edaebb1fd60b1e391a3
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-08 11:30:22 -0500

    Add exercise 16

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Defines a Deep Neural Network for binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Class DeepNeuralNetwork"""
+
+    def __init__(self, nx, layers):
+        """Constructor method"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if type(layers) is not list \
+           and all(type(x) is int and x > 0 for x in layers):
+            raise TypeError('layers must be a list of positive integers')
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for i in range(self.L):
+            W_key = "W" + str(i + 1)
+            b_key = "b" + str(i + 1)
+            if i == 0:
+                self.weights[W_key] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2/nx)
+                self.weights[b_key] = np.zeros((layers[i], 1))
+            else:
+                self.weights[W_key] = np.random.randn(
+                    layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
+                self.weights[b_key] = np.zeros((layers[i], 1))
