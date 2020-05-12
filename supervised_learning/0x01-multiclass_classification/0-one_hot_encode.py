3ff14ad93fd40b7eb55097ee1b2f3740d5d650fb
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-12 11:22:52 -0500

    Fix exercise 0

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,10 +6,12 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Performs one-hot encoding"""
-    if classes < 1:
+    if type(Y) is not np.ndarray or Y.size < 1:
+        return None
+    if type(classes) is not int or classes < 1:
         return None
 
-    hot_encoded_matrix = np.zeros((classes, Y.size))
+    hot_encoded_matrix = np.zeros((Y.size, classes))
     hot_encoded_matrix[Y, np.arange(Y.size)] = 1
     print(hot_encoded_matrix.shape)
     return hot_encoded_matrix
