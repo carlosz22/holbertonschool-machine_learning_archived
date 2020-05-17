c4e88df559e8645f546ccf45934e0435edd563c9
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-17 12:09:39 -0500

    Fix exercise 0

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -6,12 +6,8 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """Create placeholders"""
-    if type(nx) is not int or nx < 1:
-        return None
-    if type(classes) is not int or classes < 1:
-        return None
 
-    x = tf.placeholder(dtype=tf.float32, shape=(None, nx))
-    y = tf.placeholder(dtype=tf.float32, shape=(None, classes))
+    x = tf.placeholder(dtype=tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(dtype=tf.float32, shape=(None, classes), name='y')
 
     return x, y
