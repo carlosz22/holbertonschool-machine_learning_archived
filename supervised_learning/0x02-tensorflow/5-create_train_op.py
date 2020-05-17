604afcf01eeaa4638929fe43ab17b6ac3662efd4
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-17 17:19:44 -0500

    Change comments in exercise 5

--- supervised_learning/0x02-tensorflow/5-create_train_op.py
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-"""Train in TensorFlow"""
+"""Creates train operation in TensorFlow"""
 import tensorflow as tf
 
 
 def create_train_op(loss, alpha):
-    """ Train
+    """ Creates train operation
             - loss is the loss of the network’s prediction
             - alpha is the learning rate
         Returns: an operation that trains the network using gradient descent
