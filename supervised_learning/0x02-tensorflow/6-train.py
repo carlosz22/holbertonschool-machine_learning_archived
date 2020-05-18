393fd76a9ac1cee2d14c60c4bc0fa2247fc40481
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-17 22:44:10 -0500

    Fix exercise 6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -9,7 +9,6 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-
 def train(X_train, Y_train, X_valid, Y_valid,
           layer_sizes, activations, alpha,
           iterations, save_path="/tmp/model.ckpt"):
