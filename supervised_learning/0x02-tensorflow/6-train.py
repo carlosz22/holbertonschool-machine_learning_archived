c1bb17d72ce5767c1d44c2d9bdc104e08ee4ddef
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-17 22:18:35 -0500

    Fix exercise 6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -2,6 +2,12 @@
 
 """Builds, trains and saves a NN classifier in TensorFlow"""
 import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = \
+    __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid,
@@ -20,12 +26,6 @@ def train(X_train, Y_train, X_valid, Y_valid,
             - iterations is the number of iterations to train over
             - save_path designates where to save the model
     """
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = \
-        __import__('0-create_placeholders').create_placeholders
-    create_train_op = __import__('5-create_train_op').create_train_op
-    forward_prop = __import__('2-forward_prop').forward_prop
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     tf.add_to_collection('x', x)
@@ -40,7 +40,7 @@ def train(X_train, Y_train, X_valid, Y_valid,
     accuracy = calculate_accuracy(y, y_pred)
     tf.add_to_collection('accuracy', accuracy)
 
-    train_op = create_train_op(loss, alpha)   
+    train_op = create_train_op(loss, alpha)
     tf.add_to_collection('train_op', train_op)
 
     init = tf.global_variables_initializer()
